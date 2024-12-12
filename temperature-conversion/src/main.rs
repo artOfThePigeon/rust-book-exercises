afb86a0d90f3ef853ea7c6b2207be1cf0a3e2133
David <42543340+artOfThePigeon@users.noreply.github.com>
2024-12-12 12:15:14 -0600

    added celcius conversion to the temperature conversion program

--- temperature-conversion/src/main.rs
+++ temperature-conversion/src/main.rs
@@ -1,35 +1,58 @@
 use std::io;
 
 fn main() {
-    println!(
-        "Let's convert some temperatures! For now, we are going to convert Fahrenheit to Celsius."
-    );
+    println!("Let's convert some temperatures!");
 
-    'get_temp: loop {
-        println!("Please enter a temperature:");
-        let mut temperature: String = String::new();
-        io::stdin().read_line(&mut temperature).expect("error");
-        let temperature: f64 = if let Ok(num) = temperature.trim().parse() {
-            num
-        } else {
-            continue 'get_temp;
+    'temp_scale: loop {
+        println!("Do you want to convert to Celsius or Fahrenheit? Please enter the letter F or C for the scale you want to convert to.");
+        let mut temp_scale = String::new();
+
+        io::stdin().read_line(&mut temp_scale).expect("error");
+
+        let temp_scale: char = match temp_scale.trim().to_uppercase().as_str() {
+            "F" => 'F',
+            "C" => 'C',
+            _ => continue 'temp_scale,
         };
 
-        let conversion: f64 = ((temperature - 32.0) * 5.0) / 9.0;
-        println!(
-            "{}° Fahrenheit converts to {:.2}° Celsius.",
-            temperature, conversion
-        );
+        'get_temp: loop {
+            println!("Please enter a temperature:");
+            let mut temperature: String = String::new();
+            io::stdin().read_line(&mut temperature).expect("error");
+            let temperature: f64 = if let Ok(num) = temperature.trim().parse() {
+                num
+            } else {
+                continue 'get_temp;
+            };
 
+            let conversion = match temp_scale {
+                'C' => ((temperature - 32.0) * 5.0) / 9.0,
+                'F' => (temperature * 1.8) + 32.0,
+                _ => panic!("what in the hell bobby!"),
+            };
+
+            match temp_scale {
+                'C' => println!(
+                    "{}° Fahrenheit converts to {:.2}° Celsius.",
+                    temperature, conversion
+                ),
+                'F' => println!(
+                    "{}° Celsius converts to {:.2}° Fahrenheit.",
+                    temperature, conversion
+                ),
+                _ => panic!("what in the hell bobby!"),
+            };
+            break;
+        }
         println!("Would you like to convert another temperature? Y or N");
 
         'keep_playing: loop {
             let mut choice: String = String::new();
             io::stdin().read_line(&mut choice).expect("error");
 
-            match choice.trim() {
+            match choice.trim().to_uppercase().as_str() {
                 "Y" => break 'keep_playing,
-                "N" => break 'get_temp,
+                "N" => break 'temp_scale,
                 _ => {
                     println!("Please enter Y or N.");
                     continue 'keep_playing;
