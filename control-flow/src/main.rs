ed7ac0379a37936e81e30b90a0a2a91567e02f32
David <42543340+artOfThePigeon@users.noreply.github.com>
2024-12-21 15:34:59 -0600

    added catch all practice

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,4 +1,6 @@
 #[derive(Debug)]
+
+// the code below should not be taken as a complete program, but rather disparate exercises, mostly unrelated.
 enum UsState {
     Alaska,
     Alabama,
@@ -30,6 +32,33 @@ fn main() {
     }
     value_in_cents(Coin::Quarter(UsState::Alaska));
     value_in_cents(Coin::Penny);
+
+    // returning values inside Option<T>
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    // catch all patterns
+    let dice_roll = 9;
+    match dice_roll {
+        7 => add_fancy_socks(),
+        9 => remove_fancy_socks(),
+        _ => () // can use the unit type if no other code should run
+        //  _ => reroll()
+        // other => move_player(other),
+    }
+    fn add_fancy_socks() {}
+    fn remove_fancy_socks() {}
+    // fn reroll() {}
+    // fn move_player(num_spaces: u8) {}
+
 }
 
 
